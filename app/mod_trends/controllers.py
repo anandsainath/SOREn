468e49d7766b0d2ceda015855038d04356282655
Anand Sainath <anand.sainath@gmail.com>
2014-11-17 03:11:53 -0500

    Base framework for trend analysis

--- app/mod_trends/controllers.py
+++ app/mod_trends/controllers.py
@@ -1,9 +1,9 @@
 #Flask dependencies
-from flask import Blueprint, request
+from flask import Blueprint, request, render_template
 
 # Define the blueprint: 'data', set its url prefix: app.url/data
 mod_trends = Blueprint('trends', __name__, url_prefix='/trends')
 
 @mod_trends.route('/')
 def index():
-	return "Hello World!"
+	return render_template("mod_trends/index.html")

3abf674f7e8a4d27d44229f61a043cb1820297e6
Sanjay Obla <ojsanjay@gmail.com>
2014-12-04 03:43:08 -0500

    Response time prediction skeleton

--- app/mod_response_time/controllers.py
+++ app/mod_response_time/controllers.py
@@ -1,5 +1,6 @@
 #Flask dependencies
 from flask import Blueprint, request, render_template
+import json
 
 # Define the blueprint: 'data', set its url prefix: app.url/data
 mod_response_time = Blueprint('response-time', __name__, url_prefix='/questions')
@@ -11,3 +12,11 @@ def index():
 @mod_response_time.route('/ask')
 def ask():
 	return render_template("mod_response_time/ask.html")
+
+@mod_response_time.route('/get-predictions', methods=['GET','POST'])
+def get_predictions():
+	result = {}
+	result['fastest'] = 0
+	result['authoritative'] = 1
+
+	return json.dumps(result)
